326672913c5f3054697f11ebc74a4826e6474d89
Julien Albenque <julienalbenque@gmail.com>
2017-09-12 11:36:50 +0200

    tt

---
+++ cryptoArbitrage/WebsocketServer.cpp
@@ -0,0 +1,9 @@
+//
+//  WebsocketServer.cpp
+//  cryptoArbitrage
+//
+//  Created by Tristan Nicolaides on 11/09/2017.
+//  Copyright © 2017 TristanNicolaides. All rights reserved.
+//
+
+#include "WebsocketServer.hpp"
---
+++ cryptoArbitrage/WebsocketServer.hpp
@@ -0,0 +1,14 @@
+//
+//  WebsocketServer.hpp
+//  cryptoArbitrage
+//
+//  Created by Tristan Nicolaides on 11/09/2017.
+//  Copyright © 2017 TristanNicolaides. All rights reserved.
+//
+
+#ifndef WebsocketServer_hpp
+#define WebsocketServer_hpp
+
+#include <stdio.h>
+
+#endif /* WebsocketServer_hpp */
